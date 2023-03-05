b7705062db0f63fd4a1338f8d1234d8c3030e9bb
vladxdboi <vladiks232@inbox.lv>
2023-03-05 17:51:53 +0200

    bob

--- tree_height.py
+++ tree_height.py
@@ -10,10 +10,7 @@ def compute_height(n, parents):
     def depthium(node):
         if depth[node] != 0:
             return depth[node]
-
-        depth[node] = 1 + depthium(parents[node]) 
-            if parents[node] != -1 
-                else 1
+        depth[node] = 1 + depthium(parents[node]) if parents[node] != -1 else 1
         return depth[node]
     
     for i in range(n):
