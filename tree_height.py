414340a24d7f2d9e5562192e1cf523088499cda4
vladxdboi <vladiks232@inbox.lv>
2023-03-05 17:44:21 +0200

    fas

--- tree_height.py
+++ tree_height.py
@@ -3,55 +3,40 @@ import threading
 import numpy as np
 
 
-def compute_height(n, parents):
+def height(n, parents):
     parents = np.array(parents)
-    depth = np.zeros(n, dtype=int)
+    meow = np.zeros(n, dtype=int)
 
     def depth(node):
-        if (depth[node] != 0):
-            return depth[node]
-        if (parents[node] == -1):
-            depth[node] = 1
+    if meow[node] != 0:
+        return meow[node]
+    else:
+        if parents[node] == -1:
+            meow[node] = 1
         else:
-            depth[node] = 1 + depth(parents[node])
-        return depth[node]
-
+            meow[node] = 1 + depth(parents[node])
+        return meow[node]
+    
     for i in range(n):
-        depth(i)
+        meow(i)
 
-    return np.max(depth)
-
-def bob_builder(n, var):
-    tree = np.zeros((n, n), dtype=bool)
-    root = None
-
-    for i, parent in enumerate(var):
-        if parent != -1:
-            tree[parent][i] = True
-        else:
-            root = i
-
-    return tree, root
-
+    return np.max(meow)
 def main():
-    obamium = input().strip()
-    if obamium == "I":
+    obama = input().strip()
+    if (obama == "I" ):
         n = int(input())
         parents = list(map(int, input().split()))
-        tree, root = bob_builder(n, parents)
-        height = compute_height(n, parents)
-    elif obamium == "F":
+        height = height(n, parents)
+    elif (obama == "F"):
         test_number = input()
         with open(f"test/{test_number}", "r") as file:
             n = int(file.readline().strip())
             parents = list(map(int, file.readline().strip().split()))
-            tree, root = bob_builder(n, parents)
-            height = compute_height(n, parents)
+            height = height(n, parents)
     else:
         return
 
     print(height)
-
 
 sys.setrecursionlimit(10**7)
 threading.stack_size(2**27)
