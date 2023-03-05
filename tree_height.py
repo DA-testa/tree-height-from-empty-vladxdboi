31e50bd9981e658cd35f3be36930a5f3ded63764
vladxdboi <vladiks232@inbox.lv>
2023-03-05 17:30:34 +0200

    oof

--- tree_height.py
+++ tree_height.py
@@ -1,51 +1,52 @@
+import numpy as np
 import sys
 import threading
-import numpy as np
 
-def build_tree(n, parents):
-    tree = {}
-    index = 0
-    
-    for i in range(n):
-        tree[i] = []
-    
-    for i, parent in enumerate(parents):
+def bob_builder(n, var):
+    tree = np.zeros((n, n), dtype=bool)
+    root = None
+
+    for i, parent in enumerate(var):
         if parent != -1:
-            tree[parent].append(i)
+            tree[parent][i] = True
         else:
-            index = i
-            
-    return tree, index
+            root = i
 
-def compute_height(tree, index):
-    tree = [(index, 1)]
+    return tree, root
+
+
+def compute_height(tree, root):
+    queue = [(root, 1)]
     max = 0
-    while tree:
-        node, height = tree.pop(0)
-        max = max(max, height)
-        for child in tree[node]:
-            tree.append((child, height+1))
-            
+
+    while queue:
+        node, height = queue.pop(0)
+        max = np.max([max, height])
+        children = np.where(tree[node] == True)[0]
+        queue.extend([(child, height + 1) for child in children])
+
     return max
 
+
 def main():
-    obama = input().strip()
-    if (obama == "I" ):
-        n = int(input())
-        parents = list(map(int, input().split()))
-        height = compute_height(n, parents)
-    elif (obama == "F"):
-        num = input()
-        with open(f"test/{num}", "r") as file:
-            n = int(file.readline().strip())
-            parents = list(map(int, file.readline().strip().split()))
-            tree, index = build_tree(n, parents)
-            height = compute_height(tree, index)
+    input_str = input().strip()
+    if input_str == "F":
+        file = input().strip()
+        if file == "a":
+            return
+        with open(f"./test/{file}", mode="r") as obama:
+            n, *var = map(int, obama.read().split())
+            var = np.array(var)
+    elif input_str == "I":
+        n, *var = map(int, input().strip().split())
+        var = np.array(var)
     else:
         return
 
-    print(height)
+    tree, root = bob_builder(n, var)
+    print(compute_height(tree, root))
 
-    sys.setrecursionlimit(10**7) 
-    threading.stack_size(2**27) 
-    threading.Thread(target=main).start()
+
+sys.setrecursionlimit(10 ** 7)
+threading.stack_size(2 ** 27)
+threading.Thread(target=main).start()
