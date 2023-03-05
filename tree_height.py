f6e78088f5c9af62a2d3f1543b432a9e64d18743
vladxdboi <vladiks232@inbox.lv>
2023-03-05 16:55:45 +0200

    kad sis beigsies

--- tree_height.py
+++ tree_height.py
@@ -15,29 +15,32 @@ def bob_builder(n, var):
     return tree, root
 
 def compute_height(n, var):
-    heights = np.zeros(n)
-    for i in range(n):
-        node = i
-        height = 0
-        while node != -1:
-            if heights[node] != 0:
-                height += heights[node]
-                break
-            height += 1
-            node = var[node]
-        heights[i] = height
-    return int(max(heights))
+    root = np.where(var == -1)[0][0]
+    tree = [[] for _ in range(len(var))]
+    for i, parent in enumerate(var):
+        if parent != -1:
+            tree[parent].append(i)
 
-obama = input()
-if "F" in obama:
-    with open("test/" + input().strip()) as file:
-        n = int(file.readline())
-        vars = np.fromstring(file.readline(), dtype=int, sep=" ")
-        print(compute_height(n, vars))
-elif "I" in obama:
-    n = int(input())
-    var = np.fromstring(input(), dtype=int, sep=" ")
-    print(compute_height(n, var))
+    def height(joop):
+        children = tree[joop]
+        if not children:
+            return 1
+        return 1 + np.max([height(child) for child in children])
+    return height(root)
+
+def main():
+    inputz = input()
+    if "F" in inputz:
+        name = input().strip()
+        with open("test/" + name) as file:
+            n, *parents = map(int, file.readline().split())
+            parents = np.array(parents)
+            print(compute_height(n, parents))
+
+    elif "I" in inputz:
+        n, *parent = map(int, input().split())
+        parent = np.array(parent)
+        print(compute_height(n, parent))
 
 sys.setrecursionlimit(10 ** 7)
 threading.stack_size(2 ** 27)
